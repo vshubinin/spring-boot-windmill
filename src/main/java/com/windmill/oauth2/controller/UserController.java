7c14e971546c77f63b2bc875ac187a39be4591a5
Volodymyr <volodymyrjavadev@gmail.com>
2021-02-12 14:56:59 +0200

    add user endpoint

--- src/main/java/com/windmill/oauth2/controller/UserController.java
+++ src/main/java/com/windmill/oauth2/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.windmill.oauth2.controller;
 
+import com.windmill.oauth2.model.UserPojo;
+import com.windmill.oauth2.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    UserController(UserRepository repository) {
+        this.userRepository = repository;
+    }
+
+    // Aggregate root
+    // tag::get-aggregate-root[]
+    @GetMapping("/users")
+    List<UserPojo> all() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    UserPojo one(@PathVariable String id) {
+
+        return userRepository.findByUsername(id);
+    }
+
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS users (
-  username varchar(255) NOT NULL,
+CREATE TABLE users (
+  username varchar(255) UNIQUE NOT NULL,
   country varchar(255) DEFAULT NULL,
   full_name varchar(255) DEFAULT NULL,
   password varchar(255) DEFAULT NULL,
   role varchar(255) DEFAULT NULL,
   PRIMARY KEY (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 INSERT INTO users (username, country, full_name, password, role) VALUES
 	('admin', 'UK', 'Igor', '$2a$10$Z5F2Elzpnwx6kp0CYLmdo.Tcv8SZWMANvlr/PWr6.IxWWXnAi7KNC', 'ROLE_ADMIN'),
